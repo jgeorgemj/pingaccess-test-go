b27aa9fd1d4346abaadf6140a74cead6a374fb5a
jgeorgemj <jithingeorge092@gmail.com>
2022-09-29 01:38:28 -0700

    modified main and added go.mod

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jgeorgemj/pingccess-test-go
+
+go 1.16
+
+require (
+	github.com/google/uuid v1.1.1
+	github.com/stretchr/testify v1.7.0
+)
--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
   "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
   "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-  "pingaccess-test-go/pingaccess" # need to change
+  "pingaccess-test-go/pingaccess"
 )
 
 func main() {
   plugin.Serve(&plugin.ServeOpts{
     ProviderFunc: func() *schema.Provider {
-      return pingaccess.Provider() #need to change
+      return pingaccess.Provider() 
     },
   })
 }
